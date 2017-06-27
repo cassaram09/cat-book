51163b9e039f2d10e376a731670b93da47d8f079
cassaram09 <cassaram09@gmail.com>
2017-06-27 16:37:45 -0400

    add index/show controller actions for cats and hobbies

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,18 @@
 class CatsController < ApplicationController
+  
+  def index
+    @cats = Cat.all
+    render json: @cats
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render json: @cat
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:id, :name, :age)
+  end
+
 end
--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,2 +1,16 @@
 class HobbiesController < ApplicationController
+  def index
+    @hobbies = Hobby.all
+    render json: @hobbies
+  end
+
+  def show
+    @hobby = Hobby.find(params[:id])
+    render json: @hobby
+  end
+
+  private
+  def hobby_params
+    params.require(:hobby).permit(:id, :description)
+  end
 end
