d58784e7227b5bdca04f8f7b218ab11e4bdc5cc3
cassaram09 <cassaram09@gmail.com>
2017-06-27 17:33:03 -0400

    add store and supoort for API requests via thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { Router, browserHistory } from 'react-router';  
+import { Router, browserHistory } from 'react-router'; 
+import configureStore from './store/configureStore';  
+import { Provider } from 'react-redux'; 
 import routes from './routes';
 
-
+const store = configureStore();
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
    document.getElementById('root')
+
 );
 registerServiceWorker();
